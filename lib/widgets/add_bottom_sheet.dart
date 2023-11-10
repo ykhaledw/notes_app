66c0397f794529898b00df949567fa9d80342885
ykhaledw <youssefwahdan12@gmail.com>
2023-11-10 11:17:33 +0200

    create simple bloc observer

--- lib/widgets/add_bottom_sheet.dart
+++ lib/widgets/add_bottom_sheet.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
 
 class AddBottomSheet extends StatelessWidget {
@@ -6,12 +10,25 @@ class AddBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(left: 16, right: 16, bottom: 12),
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, bottom: 12),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccess) {
+              Navigator.of(context).pop();
+            } else if (state is AddNoteFailure) {
+              print('Failed ${state.errMessage}');
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
 }
-
